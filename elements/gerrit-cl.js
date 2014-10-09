57480ef4f38bce761281b2e552e0f979de2733b1
Jelte Liebrand <jliebrand@google.com>
2014-10-09 15:18:56 +0100

    Adding support for try jobs

--- elements/gerrit-cl.js
+++ elements/gerrit-cl.js
@@ -47,17 +47,60 @@ Polymer('gerrit-cl', {
         }
 
         // check if we have any try job success/failures
+        // and also check if I have made any comments at all
+        var everCommentedByMe = false;
         var tryJobCss;
+        var tryJobsPatchsets = {};
+
         for (i = 0; i < this.details.messages.length; i++) {
           var msg = this.details.messages[i];
-          if (this.details.messages[i].message.match(/try job.*FAILURE/)) {
-            tryJobCss = 'failure';
-          } else if (this.details.messages[i].message.match(/try job.*SUCCESS/)) {
-            tryJobCss = 'success';
+
+          everCommentedByMe = everCommentedByMe ||
+              (msg && msg.author &&
+               msg.author.email &&
+               msg.author.email === items.email);
+
+          var matches = msg.message.toLowerCase().match(/patch set (\d+)(.|[\r\n])*!tryjob/);
+
+          if (matches) {
+            // new request for patch set; clear old gathered data
+            tryJobsPatchsets[matches[1]] = {};
+          }
+
+          matches = msg.message.toLowerCase().match(/patch set (\d+)(.|[\r\n])*try job has (\S*).*:\s*(.*)/);
+          if (matches) {
+            var patchSet = matches[1];
+            var legacyTryJob = matches[3] === 'finished';
+            var result = matches[4];
+            tryJobsPatchsets[patchSet] = tryJobsPatchsets[patchSet] || {};
+            tryJobsPatchsets[patchSet].legacy = legacyTryJob;
+            tryJobsPatchsets[patchSet][result] = ++tryJobsPatchsets[patchSet][result] || 1;
           }
         }
+
+        // TODO(jliebrand): HACK until try jobs are better reported
+        // in gerrit; for now HARDCODE the need for 8 success messages
+        // on the highest patchset.
+        var patchSets = Object.keys(tryJobsPatchsets).sort(function(a,b) {return a-b;});
+        if (patchSets.length > 0) {
+          var highestPatchset = patchSets.pop();
+          var tryJob = tryJobsPatchsets[highestPatchset];
+
+          if (tryJob.legacy) {
+            tryJobCss = (tryJob.success === 1) ?
+              'success' : 'failure';
+          }
+          else {
+            tryJobCss = (tryJob.success === 8) ?
+              'success' : (tryJob.failure || tryJob.exception) ? 'failure' : 'pending';
+          }
+        }
+
         if (tryJobCss) {
           this.$.tryJob.classList.add(tryJobCss);
+        }
+        if (!everCommentedByMe) {
+          this.classList.add('new');
         }
       }
 
